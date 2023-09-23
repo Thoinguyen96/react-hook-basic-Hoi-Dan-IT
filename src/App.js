55ece4caf1653cb197e7fd0ea6b9be5bedf8a4f8
Thoinguyen96 <vanthoiqng@gmail.com>
2023-09-23 20:52:03 +0700

    19

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import { increaseCounter, decreaseCounter } from "./action/actions";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import { useEffect } from "react";
 import Home from "./components/Home";
 function App(props) {
-    const dispatch = useDispatch();
-
     // const newCount = useSelector((state) => state.counter.count);
     // const desc = useSelector((state) => state.counter.description);
     // const nickname = useSelector((state) => state.counter.nickname);
 
-    const handleIncrease = () => {
-        dispatch(increaseCounter());
-    };
-    const handleDecrease = () => {
-        dispatch(decreaseCounter());
-    };
-    useEffect(() => {
-        fetchAllUser();
-    }, []);
-    const fetchAllUser = async () => {
-        let res = await axios.get("http://localhost:8080/users/all");
-        const data = res && res.data ? res.data : [];
-        console.log(data);
-    };
     return (
         <div className="App">
             <Home />
--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,6 +1,27 @@
 import { Container, Table } from "react-bootstrap";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function TableUser(props) {
+    const [dataUser, setDataUser] = useState([]);
+    useEffect(() => {
+        fetchAllUser();
+    }, []);
+    const fetchAllUser = async () => {
+        let res = await axios.get("http://localhost:8080/users/all");
+
+        const data = res && res.data ? res.data : [];
+        if (data) {
+            setDataUser(data);
+            console.log(data);
+        }
+    };
+    const handleDeleteUser = (id) => {
+        const resultDelete = dataUser.filter((data) => {
+            return data.id !== id;
+        });
+        console.log(resultDelete);
+        setDataUser(resultDelete);
+    };
     return (
         <>
             <Container className="mt-3">
@@ -8,29 +29,31 @@ function TableUser(props) {
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
+                            <th>Email Address</th>
                             <th>Username</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td colSpan={2}>Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        {dataUser &&
+                            dataUser.length > 0 &&
+                            dataUser.map((data, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{data.email}</td>
+                                        <td>{data.username}</td>
+                                        <td>
+                                            <button
+                                                onClick={() => handleDeleteUser(data.id)}
+                                                className="btn btn-danger"
+                                            >
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
                     </tbody>
                 </Table>
             </Container>
